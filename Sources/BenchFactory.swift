a4564368ae2d1a9f33fdd15ebcb92f4fb973fe63
SaidSikira <saidsikira@gmail.com>
2016-01-30 20:44:47 +0100

    Added Serializer implementation

--- Sources/BenchFactory.swift
+++ Sources/BenchFactory.swift
@@ -7,14 +7,39 @@
 //
 
 import CoreCityOS
+import Flowthings
 
 public final class BenchFactory: FactoryType {
     
+    /// Factory singleton instance
     public static let sharedInstance = BenchFactory()
     
-    public init() {}
+    public init() {
+    }
     
-    public func getLatestData(completion:(data: [LiveDataCollectionType], error: ErrorType) -> ()) {
+    private var api: FTAPI = {
+        return FTAPI(
+            accountID: "ceco",
+            tokenID: "TWEFiZvO0KtvWMx5p24JvBbFhBA1oDL3"
+        )
+    }()
     
+    public func getLatestData(completion:(data: [LiveDataCollectionType]?, error: ErrorType?) -> ()) {
+        
+        let findParams = FindParams()
+        
+        self.api.drop.find("f562e8c4f68056d244d594ce6", findParams: findParams)
+            .then {
+                body -> () in
+                do {
+                    let data = try Serializer.serializeLiveData(jsonData: body)
+                    completion(data: data, error: nil)
+                } catch {
+                    throw error
+                }
+            }.error {
+                error in
+                completion(data: nil, error: error)
+        }
     }
 }
--- Sources/Serializer.swift
+++ Sources/Serializer.swift
@@ -11,7 +11,32 @@ import Flowthings
 
 class Serializer {
     
-    func serializeLiveData(jsonData json: JSON) throws -> [LiveDataCollectionType] {
-        return [BenchDataCollection()]
+    /// Serializes live data
+    class func serializeLiveData(jsonData json: JSON) throws -> [LiveDataCollectionType] {
+        
+        var dataCollection = [LiveDataCollectionType]()
+        
+        if let array = json.array {
+            let range = 0..<array.count
+            
+            for i in range {
+                let benchID = json["body"][i]["id"].string!
+                let creationDate = NSDate(timeIntervalSince1970: json["body"][i]["creationDate"].double! / 1000)
+                
+                var benchData = BenchDataCollection(deviceID: benchID, creationDate: creationDate)
+                
+                for reading in benchData.allReadings {
+                    let jsonKey = reading.jsonKey
+                    
+                    if let dataReading = json["body"][i]["elems"][jsonKey]["value"].double {
+                        benchData[jsonKey]?.addDataPoint(DataPoint(value: dataReading))
+                        
+                        dataCollection.append(benchData)
+                    }
+                }
+            }
+        }
+        
+        return dataCollection
     }
 }
